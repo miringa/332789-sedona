d921bec964d3cb311efa1fc62670d701ebedfe0e
miringa <inga.po4ta@gmail.com>
2017-02-20 17:23:11 +0300

    внесла правки, добавила js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let searchStartBnt = document.body.querySelector('.head-search');
+let searchForm = document.getElementById('search-form');
+
+searchStartBnt.addEventListener('click', function() {
+	searchForm.classList.toggle('hidden');
+});
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 34.797, lng: -112.000},
+    	scrollwheel: true,
+    	zoom: 10
+    });
+}
